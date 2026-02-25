d882e825a4ab5a0014bc353667ccbc01c1f2e7fa
jonathan-code <jonathanmutombo220@gmail.com>
2026-02-25 16:16:38 +0100

    service commit 2

---
+++ lib/firebase_options.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyBE59VpNG5BOs6ZkkdpV3e92-S7hampNO0",
+      appId: "1:595540566419:web:f863627f56b21a1aaf0866",
+      messagingSenderId: "595540566419",
+      projectId: "budget-app-a7a07",
+      authDomain: "budget-app-a7a07.firebaseapp.com",
+      storageBucket: "budget-app-a7a07.firebasestorage.app",
+    );
+  }
+}
